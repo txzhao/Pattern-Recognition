e7bcf43891d9111d26a24376787aa2fb317dd7e4
txzhao <txzhao@n188-p68.eduroam.kth.se>
2017-09-22 14:12:59 +0200

    add multiple trials

--- assignment_1/testCasePara.m
+++ assignment_1/testCasePara.m
@@ -16,6 +16,11 @@ switch test_name
         f2 = 'A';   A = [0.99 0.01 0.005; 0.03 0.97 0.002];
         f3 = 'b1';  b1 = GaussD('Mean', 0, 'StDev', 1);
         f4 = 'b2';  b2 = GaussD('Mean', 3, 'StDev', 2);
+    case 'vector-output HMM'
+        f1 = 'q';   q = [0.75; 0.25];
+        f2 = 'A';   A = [0.99 0.01; 0.03 0.97];
+        f3 = 'b1';  b1 = GaussD('Mean', [0; 0], 'Covariance', [2 1; 1 4]);
+        f4 = 'b2';  b2 = GaussD('Mean', [3; 3], 'Covariance', [2 0; 0 4]);
     otherwise
         warning('Wrong test case name!')
 end
--- assignment_1/verify.m
+++ assignment_1/verify.m
@@ -6,31 +6,47 @@ mc = MarkovChain(para.q, para.A);
 h = HMM(mc, [para.b1; para.b2]);
 
 %% verify MarkovChain rand method
-T = 10000;
-S = rand(mc, T); 
-f_S = zeros(1, length(para.q));
+T = 10000; 
+n_trial = 10;
+f_S = zeros(n_trial, length(para.q));
 
 % calculate relative frequency of occurrences
-disp('------- verify MarkovChain rand method -------');
-for i = 1 : length(para.q)
-    f_S(i) = sum(S == i)/length(S);
-    disp(['Freq of St = ', num2str(i), ': ', num2str(f_S(i))]);
+for n = 1 : n_trial
+    S = rand(mc, T);
+    for i = 1 : length(para.q)
+        f_S(n, i) = sum(S == i)/length(S);
+    end
 end
+
+% output and view results
+disp(['------- verify MarkovChain rand method with ' num2str(n_trial) ' trials -------']);
+figure;
+for i = 1 : length(para.q)
+    disp(['Average freq of St = ', num2str(i), ': ', num2str(sum(f_S(:, i))/n_trial)]);
+    subplot(2, 1, i)
+    scatter(1 : n_trial, f_S(:, i));
+    title(['Relative frequency of S = ' num2str(i) ' with respect to index of trials'])
+    xlabel('Index of trials')
+    ylabel('Relative frequency')
+end
+
 fprintf('\r');
 
 %% verify HMM rand method 
 nSamples = 10000;
 [X, ~] = rand(h, nSamples);
-mu = mean(X);
-va = var(X);
+mu = mean(X, 2);
+va = var(X, 0, 2);
 disp('------- verify HMM rand method -------');
-disp(['Mean of Xt: ', num2str(mu)]);
-disp(['Var of Xt: ', num2str(va)]);
+disp(['Mean of Xt: ', num2str(mu')]);
+disp(['Var of Xt: ', num2str(va')]);
 fprintf('\r');
+
 
 %% HMM impression
 nSamples = 500;
 [X, S] = rand(h, nSamples);
+figure;
 subplot(2, 1, 1);
 plot(1:length(S), S);
 title('Plot of state St with respect to time step t')
@@ -41,7 +57,4 @@ plot(1:length(X), X);
 title('Plot of contiguous samples Xt with respect to time step t')
 xlabel('t')
 ylabel('Xt')
-
-
-
 
