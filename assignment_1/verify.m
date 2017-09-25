3c06e7964b71f229fe6275950ae799aaa52c9569
txzhao <txzhao@txzhaodeMacBook-Air.local>
2017-09-25 18:08:33 +0200

    average means and variances

--- assignment_1/verify.m
+++ assignment_1/verify.m
@@ -1,13 +1,13 @@
 clear;
 
 %% build up HMM sources
-para = testCasePara('regular HMM');
+para = testCasePara('same-mean HMM');
 mc = MarkovChain(para.q, para.A);
 h = HMM(mc, [para.b1; para.b2]);
 
 %% verify MarkovChain rand method
 T = 10000; 
-n_trial = 10;
+n_trial = 3;
 f_S = zeros(n_trial, length(para.q));
 
 % calculate relative frequency of occurrences
@@ -34,18 +34,31 @@ fprintf('\r');
 
 %% verify HMM rand method 
 nSamples = 10000;
-[X, ~] = rand(h, nSamples);
-mu = mean(X, 2);
-va = var(X, 0, 2);
-disp('------- verify HMM rand method -------');
-disp(['Mean of Xt: ', num2str(mu')]);
-disp(['Var of Xt: ', num2str(va')]);
+n_trial = 5;
+Mu = zeros(length(para.b1.Mean), 1);
+Var = zeros(length(para.b1.Mean), 1);
+
+% run repeatedly and average mean and variance
+for n = 1 : n_trial
+    [X, ~] = rand(h, nSamples);
+    mu = mean(X, 2);
+    va = var(X, 0, 2);
+    Mu = Mu + mu;
+    Var = Var + va;
+end
+
+% output results
+disp(['------- verify HMM rand method with ' num2str(n_trial) ' trials-------']);
+disp(['Average mean of Xt: ', num2str(mu')]);
+disp(['Average var of Xt: ', num2str(va')]);
 fprintf('\r');
 
 
 %% HMM impression
 nSamples = 500;
 [X, S] = rand(h, nSamples);
+
+% view results
 figure;
 subplot(2, 1, 1);
 plot(1:length(S), S);
@@ -57,4 +70,23 @@ plot(1:length(X), X);
 title('Plot of contiguous samples Xt with respect to time step t')
 xlabel('t')
 ylabel('Xt')
+
+%% verify finite HMM
+nSamples = 500;
+n_trial = 2000;
+max_steps = 10;
+S_len_prob = zeros(1, nSamples);
+
+% calculate exiting probabilities after different steps
+for n = 1 : n_trial
+    [X, S] = rand(h, nSamples);
+    S_len_prob(length(S)) = S_len_prob(length(S)) + 1;
+end
+S_len_prob = S_len_prob/n_trial; 
+
+% output results
+disp('------- verify finite HMM -------');
+for i = 1 : max_steps
+    disp(['Prob of sequence length = ' num2str(i) ': ' num2str(S_len_prob(i))]);
+end
 
